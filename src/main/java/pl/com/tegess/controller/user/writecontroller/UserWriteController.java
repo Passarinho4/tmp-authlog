6916a9ec5093e6986cf47e89be97a874e56e31e7
Passarinho <passarinho4@gmail.com>
2016-05-11 21:32:45 +0200

    add delete user endpoint

--- src/main/java/pl/com/tegess/controller/user/writecontroller/UserWriteController.java
+++ src/main/java/pl/com/tegess/controller/user/writecontroller/UserWriteController.java
@@ -1,7 +1,11 @@
 package pl.com.tegess.controller.user.writecontroller;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.com.tegess.domain.user.UserRepository;
 
@@ -12,4 +16,9 @@ public class UserWriteController {
     @Autowired
     UserRepository repository;
 
+
+    @RequestMapping(value = "/api/users/:id", method = RequestMethod.DELETE)
+    public void deleteUser(@RequestParam String id) {
+        repository.delete(new ObjectId(id));
+    }
 }
