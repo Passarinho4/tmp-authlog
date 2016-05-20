4b4e4a4278a615a6ba424ed93976382a6d605b27
Passarinho <passarinho4@gmail.com>
2016-05-20 20:14:54 +0200

    add remove application endpoint

--- src/main/java/pl/com/tegess/controller/application/writecontroller/ApplicationWriteController.java
+++ src/main/java/pl/com/tegess/controller/application/writecontroller/ApplicationWriteController.java
@@ -2,11 +2,9 @@ package pl.com.tegess.controller.application.writecontroller;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.com.tegess.controller.application.request.ApplicationData;
 import pl.com.tegess.domain.application.Application;
 import pl.com.tegess.domain.application.ApplicationRepository;
@@ -27,5 +25,14 @@ public class ApplicationWriteController {
                 applicationData.getFacebookRedirectURI());
 
         repository.insert(application);
+    }
+
+    @RequestMapping(value = "api/applications/:id", method = RequestMethod.DELETE)
+    public void deleteApplication(@RequestParam String id) {
+        if(ObjectId.isValid(id)){
+            repository.delete(new ObjectId(id));
+        }else {
+            throw new IllegalArgumentException("Wrong id!");
+        }
     }
 }
