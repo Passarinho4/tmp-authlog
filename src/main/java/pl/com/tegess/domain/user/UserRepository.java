26333e658007cddb99007eeef0892644bc5be639
Passarinho <passarinho4@gmail.com>
2016-06-12 00:01:01 +0200

    fix

--- src/main/java/pl/com/tegess/domain/user/UserRepository.java
+++ src/main/java/pl/com/tegess/domain/user/UserRepository.java
@@ -2,6 +2,7 @@ package pl.com.tegess.domain.user;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface UserRepository extends MongoRepository<User, ObjectId> {
 
     List<User> findByUsername(String username);
 
+    @Query(value = "{'username' : ?0, 'appId' : ?1}")
     User findByUsernameAndAppId(String username, String appId);
 }
