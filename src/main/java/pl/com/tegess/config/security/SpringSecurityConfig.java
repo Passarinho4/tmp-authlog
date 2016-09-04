3509ad18853d174006659132675df490e1d2756e
Passarinho <passarinho4@gmail.com>
2016-09-04 15:36:24 +0200

    add security endpoint

--- src/main/java/pl/com/tegess/config/security/SpringSecurityConfig.java
+++ src/main/java/pl/com/tegess/config/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package pl.com.tegess.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,6 +36,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/login/facebook").permitAll()
                 .antMatchers("/api/login/credentials").permitAll()
                 .antMatchers("/api/logged/facebook").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/applications/*/users").permitAll()
                 .anyRequest().authenticated().and()
                 .addFilterBefore(statelessLoginFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(statelessAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
