c66e709f2a210d2d976419dd40d6d2b30d3b8f5b
Passarinho <passarinho4@gmail.com>
2016-09-27 22:26:07 +0200

    add getUser endpoint

--- src/main/scala/com/tegess/controller/user/UserReadController.scala
+++ src/main/scala/com/tegess/controller/user/UserReadController.scala
@@ -1,18 +1,24 @@
 package com.tegess.controller.user
 
+import com.sun.net.httpserver.Authenticator.Failure
+import com.tegess.config.newsecurity.TokenService
+import com.tegess.controller._
 import com.tegess.controller.user.UserReadController.UserData
 import com.tegess.persistance.service.user.UserService
 import org.bson.types.ObjectId
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.{PathVariable, RequestMapping, RequestMethod, RestController}
+
+import scala.util.{Failure, Success, Try}
+
 
 @RestController
 @Component
 class UserReadController {
 
-  @Autowired
-  var userService:UserService = _
+  @Autowired var userService:UserService = _
 
   @RequestMapping(value=Array("/api/applications/{appId}/users"), method=Array(RequestMethod.GET))
   def getUsers(@PathVariable appId: String) = {
@@ -24,6 +30,22 @@ class UserReadController {
       user <- users
     } yield UserData(user.username, user.username, user.mail, user.pictureUrl,
       user.gender, user.locale.map(_.toLanguageTag), user.privileges)
+
+  }
+
+  @PreAuthorize(value = "authentication.getName() == #appId.concat(#userId)")
+  @RequestMapping(value=Array("/api/applications/{appId}/users/{userId}"), method=Array(RequestMethod.GET))
+  def getUser(@PathVariable appId: String, @PathVariable userId: String) = {
+    val userTry = for {
+      applicationId <- Try(new ObjectId(appId))
+      user <- userService.findOne(applicationId, userId).toTry
+    } yield UserData(user.username, user.username, user.mail, user.pictureUrl,
+      user.gender, user.locale.map(_.toLanguageTag), user.privileges)
+
+    userTry match {
+      case Success(u) => u
+      case Failure(e) => throw new IllegalArgumentException("Something went wrong", e)
+    }
 
   }
 
